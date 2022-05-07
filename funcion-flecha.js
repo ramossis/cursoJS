850aa98d3687859a9097f24cee6edf388bbe3944
Ronaldo Ramos <rangerskiper@gmail.com>
2022-05-07 12:06:03 -0400

    pruebas y practicas JS

---
+++ funcion-flecha.js
@@ -0,0 +1,13 @@
+
+/*const sumarDos=(num1,num2)=>{
+    let sumaTotal=num1+num2;
+    return sumaTotal;
+}
+console.log(sumarDos(2,3));
+
+const vec=[1,2,3,4,5];
+vec.forEach(elem=>{
+    console.log(elem);
+});
+console.log(vec);
+*/
---
+++ var-let-const.js
@@ -0,0 +1,3 @@
+var edad=18;
+var edad=20;
+console.log(edad);
